d42e2868fd65192bf56ec1ce0df777e5e8359529
catherinerlopezv <catherinerlopezv@gmail.com>
2025-03-03 18:40:39 -0600

    primer commit

---
+++ HolaMundo.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Style, init
+
+#Inicializa color 
+init()
+
+print(Fore.MAGENTA+ "¡Hola, estrellitas! La tierra les dice hola."+ Style.RESET_ALL )
+print("Hola mundo x2")
+print("Hola mundo x3")
+print("Se agrego el texto2")
